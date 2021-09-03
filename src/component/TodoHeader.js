5783547b690471630898b7454ce91425f518c009
eroe <jooyy1219@naver.com>
2021-09-03 14:02:31 +0900

    commit

--- src/component/TodoHeader.js
+++ src/component/TodoHeader.js
@@ -1,19 +1,30 @@
-function TodoHeader(){
+import header from '../css/header.module.css';
 
-    return(
-        <header className="header">
-        <div className="header-section">
-            <h1 className="header-logo">TODO APP</h1>
-            <div className="header-form">
-                <button type="button" className="header-form-btn">
-                    <img src="add_button.svg" alt="" class="header-form-img" />
-                </button>
-                <input type="text" className="header-form-text" placeholder="새 할 일을 입력해주세요."/>
-            </div>
+function TodoHeader({ todoContent, changeTodo, addTodo }) {
+  return (
+    <header id={header.header}>
+      <div className={header.section}>
+        <h1 className={header.logo}>TODO APP</h1>
+        <div className={header.form}>
+          <button
+            type="button"
+            className={header['form-btn']}
+            onClick={addTodo}
+          >
+            <img src="add_button.svg" alt="" className={header['form-img']} />
+          </button>
+          <input
+            type="text"
+            className={header['form-text']}
+            placeholder="새 할 일을 입력해주세요."
+            onChange={changeTodo}
+            name="todoContent"
+            value={todoContent}
+          />
         </div>
+      </div>
     </header>
-    );
-
+  );
 }
 
 export default TodoHeader;
